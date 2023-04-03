8d5b4d2b2b09637ef3234a0f814cbde3a0c28d38
rabbaimehdi <48589990+rabbaimehdi@users.noreply.github.com>
2023-04-03 22:32:57 +0000

    add assets_manager

---
+++ chat_gpt/lib/constants/assets_manager.dart
@@ -0,0 +1,5 @@
+class AssetsManager{
+
+  static String openAiIcon = "assets/images/openAi.jpg";
+  static String userIcon = "assets/images/user.png";
+}
--- chat_gpt/lib/screens/chat_screen.dart
+++ chat_gpt/lib/screens/chat_screen.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
+import '../constants/strings.dart';
+import '../constants/assets_manager.dart';
+
+
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(Strings.appName),
+        leading: Image.asset(AssetsManager.openAiIcon),
+        centerTitle: true,
+      ),
+
+    );
   }
 }
